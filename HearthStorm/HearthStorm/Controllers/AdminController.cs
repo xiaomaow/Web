3fdb44097702f7d3da34c9952fe1801ac0e7e49c
xiaomaow <xiaomaow>
2015-07-23 23:59:39 +0800

    简单的查询管理员列表 但是连接数据库上貌似有问题

---
+++ HearthStorm/HearthStorm/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using HearthStorm.Models;
+
+namespace HearthStorm.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private DBContext _db = new DBContext();
+        //
+        // GET: /Admin/
+
+        /// <summary>
+        /// 获得管理员列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AdminList()
+        {
+            try
+            {
+                var AdminList = (from entry in _db.AdminEntries select entry);
+                ViewBag.Entries = AdminList.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return View();
+        }
+
+    }
+}
